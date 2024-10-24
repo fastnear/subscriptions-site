08d0e05b8b215c38c09c4fca39ad98a3824b50ec
Mike Purvis <mikedotexe@gmail.com>
2024-10-24 10:21:00 -0700

    small fixes in tailwind config for animations and syntax

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,8 @@ const config: Config = {
       },
       keyframes: {
         textGlow: {
-          '0%, 100%': { opacity: 1, filter: 'brightness(200%)' },
-          '50%': { opacity: 0.8, filter: 'brightness(300%)' },
+          '0%, 100%': { opacity: '1', filter: 'brightness(200%)' },
+          '50%': { opacity: '0.8', filter: 'brightness(300%)' },
         },
         squish: {
           '0%, 100%': { transform: 'scaleX(1)' },
@@ -30,12 +30,13 @@ const config: Config = {
         },
       },
       animation: {
-        'text-glow': 'textGlow 2s ease-in-out infinite',
-        'fast-squish': 'squish 1.5s ease-in-out 1',
-        'move-right-delay': 'moveRight 0.3s ease-in-out 1.4s forwards',
+        'text-glow': 'textGlow 3s ease-in-out infinite',
+        'fast-squish': 'squish 0.6s ease-in 1',
+        'move-right-delay': 'moveRight 0.3s ease-out 0.6s forwards',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
