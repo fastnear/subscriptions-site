1cde5bfb557ac27e1f002215ff182c80598d7207
Mike Purvis <mikedotexe@gmail.com>
2024-10-24 10:20:36 -0700

    add /pricing and /pricing/[plan] pages (free | premium)

---
+++ src/app/pricing/[plan]/page.tsx
@@ -0,0 +1,51 @@
+// the detail page for a given plan
+// currently 'free' | 'premium'
+// to add more, see plan-types.ts
+
+import { Metadata } from "next";
+
+type PricingParams = {
+    params: {
+        plan: 'free' | 'premium';
+    };
+};
+
+export default async function PricingPlanDetailsPage({ params }: PricingParams) {
+    // interesting await syntax recommended
+    // See: https://nextjs.org/docs/messages/sync-dynamic-apis#possible-ways-to-fix-it
+    const { plan: planType } = await params
+    // console.log('aloha planType', planType)
+
+    return (
+        <div className="p-8">
+            <h1 className="text-4xl font-bold">Pricing</h1>
+            <p className="mt-4">
+                Pricing detail page for: CHATGPT HELP PLS
+            </p>
+        </div>
+    );
+}
+
+// dynamic metadata
+// See: https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export async function generateMetadata({ params }: PricingParams): Promise<Metadata> {
+    const { plan: planType } = await params
+    // console.log('alohaz planType', planType)
+
+    let planTitle = 'Invalid plan'
+    switch (planType) {
+        case 'free':
+            planTitle = 'Free plan'
+            break;
+        case 'premium':
+            planTitle = 'Premium Services'
+            break;
+    }
+
+    const metadata: Metadata = {
+        title: `${planTitle} | FASTNEAR`,
+        description: 'Pricing plans and blazing fast subscription options.',
+    }
+
+    return metadata
+}
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,4 +1,23 @@
-// `app/page.tsx` is the UI for the `/` URL
-export default function Page() {
-    return <h1>Hello, Home page!</h1>
+import type { Metadata } from 'next'
+
+export default function PricingOptionsPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text-4xl font-bold">Pricing</h1>
+            <p className="mt-4">
+                Welcome to the pricing page. Here, you can find our subscription options and details about each plan.
+            </p>
+        </div>
+    );
+}
+
+// dynamic metadata
+// See: https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export async function generateMetadata(): Promise<Metadata> {
+    const metadata: Metadata = {
+        title: 'Pricing | FASTNEAR',
+        description: 'Pricing plans and blazing fast subscription options.',
+    }
+
+    return metadata
 }
