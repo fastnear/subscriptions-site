034b0e724170e4a03d64ae3e642509b1026178d8
Mike Purvis <mikedotexe@gmail.com>
2024-10-29 23:51:33 -0700

    css and cleanup

--- src/app/_components/fastnear-logo.tsx
+++ src/app/_components/fastnear-logo.tsx
@@ -3,12 +3,10 @@
 export default function FastNearLogo() {
     return (
         <div className="flex items-center justify-center">
-            {/*<div className="flex items-center justify-center h-screen bg-black">*/}
             <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 animate-text-glow">
                 FAST
             </h1>
             <div className="text-6xl font-extrabold text-pink-500 animate-fast-squish">
-                {/*<div className="text-6xl font-extrabold text-pink-500 animate-near-flip">*/}
                 NEAR
             </div>
             <div className="text-4xl md:text-4xl font-medium text-white transform -rotate-3 animate-move-right-delay">
