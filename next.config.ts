9722dba1616df9bc41ad883b9ee2a6b965093676
Mike Purvis <mikedotexe@gmail.com>
2024-11-09 01:28:14 -0800

    don't load css files bro and stuff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Prevent CSS files from being loaded on the server side
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.css$/,
+        use: "null-loader",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
